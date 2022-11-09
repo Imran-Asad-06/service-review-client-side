cd3e3e7e50329c0313aa049a4a2d7135afa02edf
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-09 21:48:45 +0600

    added service card

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='text-center mb-4'>
+            <div className="text-2xl font-bold text-orange-600">Our Services</div>
+        </div>
+    );
+};
+
+export default Services;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwXC1-Q3qgtFBjJNOe7vA_5MeQ2IZExek",
+  authDomain: "photography-assignment-6bebd.firebaseapp.com",
+  projectId: "photography-assignment-6bebd",
+  storageBucket: "photography-assignment-6bebd.appspot.com",
+  messagingSenderId: "429392388599",
+  appId: "1:429392388599:web:b73ffd8d34cd2d73644ef2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
